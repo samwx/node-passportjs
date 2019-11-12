0535d4125fa98e3148f891b2c02e84f8213830a4
samwx <samuelmartins.sw@gmail.com>
2019-11-12 13:41:01 -0300

    Add logout function

--- routes/logout.js
+++ routes/logout.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    req.logout();
     res.send('Logging out...');
+    res.redirect('/');
 });
 
 module.exports = router;
