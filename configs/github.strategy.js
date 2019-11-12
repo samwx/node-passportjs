87941c9094e36cb9e7cd03a65a435cb06ed3d5a0
samwx <samuelmartins.sw@gmail.com>
2019-11-12 13:25:53 -0300

    Changed variable names

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -1,20 +1,35 @@
-const passport = require('passport');
-const GitHubStrategy = require('passport-github').Strategy;
+const passport = require("passport");
+const GitHubStrategy = require("passport-github").Strategy;
 
-passport.use(new GitHubStrategy({
-    clientID: 'GITHUB_CLIENT_ID',
-    clientSecret: 'GITHUB_CLIENT_SECRET',
-    callbackURL: "http://localhost:3000/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    return cb(null, profile);
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: "GITHUB_CLIENT_ID",
+      clientSecret: "GITHUB_CLIENT_SECRET",
+      callbackURL: "http://localhost:3000/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const onError = () => {
+        console.log('Ocorreu um erro!')
+      }
+
+      return done(onError, profile);
+    }
+  )
+);
+
+passport.serializeUser(function(user, done) {
+  const onError = () => {
+    console.log('Ocorreu um erro!')
   }
-));
 
-passport.serializeUser(function(user, cb) {
-    cb(null, user);
+  done(onError, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
+passport.deserializeUser(function(user, done) {
+  const onError = () => {
+    console.log('Ocorreu um erro!')
+  }
+
+  done(onError, user);
 });
